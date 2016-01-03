f9bce2b5b4c528ee078d7358b77d16a39cd623d2
AlejandroG <tylez87@gmail.com>
2016-01-03 12:44:13 +0100

    Vista Home operativa.
    Vista home terminada con todos sus módulos y carrusel funcionando
    correctamente.

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -14,17 +14,11 @@
 	#menu-top {background-color: #008bb9; color: #FFFFFF; font-size: 21px;}
 	#menu-down {background-color: #008bb9; color: #FFFFFF; font-size: 15px;}
 	#footer-somos {font-size: 14px;}
-	#menu-selected{background-color: #f8f8f8; color: #008bb9; font-size: 21px;}
+	#menu-selected {background-color: #f8f8f8; color: #008bb9; font-size: 21px;}
+	#menu-categorias {color: #008bb9; font-size: 20px;}
 	h4 {color: #B3B3B3;}
+	h3 {color: #008bb9;}
 </style>
-	
-<script>
-    $(document).ready(function(){
-        $('.myCarousel').carousel({
-            interval: 3000
-        });
-    });
-</script>
 
 </head>
 
@@ -35,69 +29,150 @@
 <body>
 	<nav class="container navbar navbar-default nabar-static-top">
 	<ul class="nav nav-pills">
-		<li><a class="navbar-brand" href="index.php"><img height="32" width="90" src="assets/images/logo.png"></a></li>
+		<li><a class="navbar-brand" href="/verkami/index.php"><img height="32" width="90" src="/verkami/assets/images/logo.png"></a></li>
 		<li><a id="menu-top"> | </a></li>
-		<li><a id="menu-top" href="index.php"> Proyectos</a></li>
+		<li><a id="menu-top" href="/verkami/index.php"> Proyectos</a></li>
 		<li><a id="menu-top" href="#"> Qué es Verkami</a></li>
 		<li><a id="menu-top" href="#"> FAQ</a></li>
 		<li><a id="menu-top"> | </a></li>
 		<li><a id="menu-top" href="#"> Empieza tu proyecto</a></li>
-		<li><a id="menu-top" href="#"> Mi Perfil</a></li>
-		<li><a id="menu-top" href="#"> Iniciar Sesión/Cerrar</a></li>
+		<li><a id="menu-top" class="btn btn-default" data-toggle="modal"> Iniciar Sesión</a></li>
 	</ul>
 </nav>
 
+
+
 <main class="container">
 
-<div id="myCarousel" class="carousel slide">
-	<ol class="carousel-indicators">
-	  <li data-target="#myCarousel" data-slide-to="0" class=""></li>
-	  <li data-target="#myCarousel" data-slide-to="1" class=""></li>
-	  <li data-target="#myCarousel" data-slide-to="2" class=""></li>
-	</ol>
-	
-	<div class="carousel-inner">
-	  <div class="item active left">
-	    <img src="assets/images/carrusel_home/imagen1.png" alt="">
-	    <div class="carousel-caption">
-	      <h4>First Thumbnail label</h4>
-	  	</div>
-	  </div>
-	  
-		<div class="item next left">
-	    <img src="assets/images/carrusel_home/imagen1.png" alt="">
-	    <div class="carousel-caption">
-	      <h4>First Thumbnail label</h4>
-	  	</div>
-	  </div>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="2000">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
 
-	  <div class="item next left">
-	    <img src="assets/images/carrusel_home/imagen2.jpg" alt="">
-	    <div class="carousel-caption">
-	      <h4>First Thumbnail label</h4>
-	  	</div>
-	  </div>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="/verkami/assets/images/carrusel_home/imagen1.png" alt="...">
+      <div class="carousel-caption">
+      	<h3>Caption Text</h3>
+      </div>
+    </div>
+    <div class="item">
+      <img src="/verkami/assets/images/carrusel_home/imagen2.jpg" alt="...">
+      <div class="carousel-caption">
+      	<h3>Caption Text</h3>
+      </div>
+    </div>
+    <div class="item">
+      <img src="/verkami/assets/images/carrusel_home/imagen3.png" alt="...">
+      <div class="carousel-caption">
+      	<h3>Caption Text</h3>
+      </div>
+    </div>
+  </div>
 
-	  <div class="item next left">
-	    <img src="assets/images/carrusel_home/imagen3.jpg" alt="">
-	    <div class="carousel-caption">
-	      <h4>First Thumbnail label</h4>
-	  	</div>
-	  </div>
-	</div>
-	
-	<a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
-	<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div> <!-- Carousel -->
+<br>
+
+<div class="container">
+	<h3>Destacados</h3>
+	<div  align="center" style='height: 350px; width: 33%; float: left; background-color: #FFFFFF;'>
+		<br>
+		<a href=""><img src="/verkami/assets/images/carrusel_home/imagen1.png" alt=""></a>
+		<h3>Titulo</h3>
+		<p>Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción
+			 Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción</p>
+	</div> 
+
+	<div align="center" style='height: 350px; width: 33%; float: left;background-color: #FFFFFF;'>
+		<br>
+		<a href=""><img src="/verkami/assets/images/carrusel_home/imagen1.png" alt=""></a>
+		<h3>Titulo</h3>
+		<p>Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción
+		   Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción</p>
+	</div> 
+
+	<div align="center" style='height: 350px; width: 33%; float: left;background-color: #FFFFFF;'>
+		<br>
+		<a href=""><img src="/verkami/assets/images/carrusel_home/imagen1.png" alt=""></a>
+		<h3>Titulo</h3>
+		<p>Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción
+		   Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción</p>
+	</div> 
+</div>
+
+
+<div class="container">
+	<h3>Categorias</h3>
+	<div  align="center" style='height: 250px; width: 33%; float: left; background-color: #333333;'>
+		<br><br>
+		<a id="menu-categorias" href="">verkami.com/alimentación</a><br><br>
+		<a id="menu-categorias" href="">verkami.com/ciencia-y-tencologia</a><br><br>
+		<a id="menu-categorias" href="">verkami.com/diseño</a><br><br>
+		<a id="menu-categorias" href="">verkami.com/juegos</a>
+	</div> 
+
+	<div align="center"  style='height: 250px; width: 33%; float: left;background-color: #333333;'>
+		<br><br>
+		<a id="menu-categorias" href="">verkami.com/arte</a><br><br>
+		<a id="menu-categorias" href="">verkami.com/comunitario</a><br><br>
+		<a id="menu-categorias" href="">verkami.com/film</a><br><br>
+		<a id="menu-categorias" href="">verkami.com/música</a>
+	</div> 
+
+	<div align="center"  style='height: 250px; width: 33%; float: left;background-color: #333333;'>
+		<br><br>
+		<a id="menu-categorias" href="">verkami.com/artes-escenicas</a><br><br>
+		<a id="menu-categorias" href="">verkami.com/comic</a><br><br>
+		<a id="menu-categorias" href="">verkami.com/fotografía</a><br><br>
+		<a id="menu-categorias" href="">verkami.com/publicaciones</a>
+	</div> 
 </div>
 <br>
 
+<div class="container">
+	<h3>En Curso</h3>
+	<div  align="center" style='height: 350px; width: 33%; float: left; background-color: #FFFFFF;'>
+		<br>
+		<a href=""><img src="/verkami/assets/images/carrusel_home/imagen1.png" alt=""></a>
+		<h3>Titulo</h3>
+		<p>Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción
+			 Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción</p>
+	</div> 
+
+	<div align="center" style='height: 350px; width: 33%; float: left;background-color: #FFFFFF;'>
+		<br>
+		<a href=""><img src="/verkami/assets/images/carrusel_home/imagen1.png" alt=""></a>
+		<h3>Titulo</h3>
+		<p>Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción
+		   Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción</p>
+	</div> 
+
+	<div align="center" style='height: 350px; width: 33%; float: left;background-color: #FFFFFF;'>
+		<br>
+		<a href=""><img src="/verkami/assets/images/carrusel_home/imagen1.png" alt=""></a>
+		<h3>Titulo</h3>
+		<p>Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción
+		   Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción</p>
+	</div> 
+</div>
 
 </main>
 
 
 <footer class="container navbar-default">
 	<div class="col-sm-2" align="center" >
-		<a href="index.php"><img height="62" width="170" src="assets/images/logo.png"></a>
+		<a href="index.php"><img height="62" width="170" src="/verkami/assets/images/logo.png"></a>
 		<p>2016 VERKAMI & CO.</p>
 	</div>
 	<div class="col-sm-8">
@@ -108,7 +183,7 @@
 	<div class="col-sm-2">
 		<nav class="navbar-right" >
 			<ul>
-				<a id="menu-down" href="index.php"> Verkami</a><br>
+				<a id="menu-down" href="/verkami/index.php"> Verkami</a><br>
 				<a id="menu-down" href="#"> Proyectos</a><br>
 				<a id="menu-down" href="#"> FAQ</a><br>
 				<a id="menu-down" href="#"> Nuevo Proyecto    </a><br>
