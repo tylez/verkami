0526c9e4d10100a653b0dac73a4996a1683f753f
Pascual Maestre Server <pascual.maestreserver@gmail.com>
2016-01-10 14:45:46 +0100

    Ampliación de plantilla
    - Añadir Scripts
    - Añadir enlaces NavBar
    - Añadir Div modal
    - Modificar direcciones de JS

--- application/views/plantilla.php
+++ application/views/plantilla.php
@@ -4,8 +4,57 @@
 	<meta charset="utf-8">
 	<title>Verkami</title>
 
-	<!--Bootstrap css-->
-	<link rel="stylesheet" type="text/css" href="<?= base_url()?>/bootstrap/css/bootstrap.css">
+	<!--Bootstrap css and JQuery-->
+  	<link rel="stylesheet" media="screen" type="text/css" href="<?= base_url()?>/bootstrap/css/bootstrap.css">
+ 	<script type="text/javascript" src="/verkami/bootstrap/js/jquery.js"></script>
+ 	<script type="text/javascript" src="/verkami/bootstrap/js/bootstrap.min.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+    
+      //La función para oculta los menus de la parte privada o publica según si estamos logueados o no.
+      function cambioaPublicoPrivado(){
+        if(localStorage.getItem("usuario") == null){
+          document.getElementById('cerrarsesion').style.display='none';  
+          document.getElementById('miperfil').style.display='none';
+          document.getElementById('iniciarseseion').style.display='inline';
+        }else{
+          document.getElementById('iniciarseseion').style.display='none';
+          document.getElementById('cerrarsesion').style.display='inline';  
+          document.getElementById('miperfil').style.display='inline';
+        }
+      }
+      cambioaPublicoPrivado();
+      //Se lleva a esta función para ocultar los menus de la parte privada o publica según si estamos logueados o no
+
+      $("#cerrarsesion").click(function(evento){
+        localStorage.removeItem("usuario");
+        cambioaPublicoPrivado();
+      });
+
+      $("#login").click(function(evento){
+          $("#erroruser").hide();
+          $("#errorpass").hide();
+          email = $("#email").val();
+          pass = $("#password").val();
+
+          if(email == "" || pass == ""){
+            if(email == ""){
+              $("#erroruser").show();
+            }
+            if(pass == ""){
+              $("#errorpass").show();                        
+            }
+          }else{
+            //comprobar que existe el email en la BD y si es asi se guarda en localstorage
+            localStorage.setItem("usuario", email);
+            cambioaPublicoPrivado();
+            $("#iniciarSesion").modal('hide');
+            
+          }
+      });
+    });
+</script>
 
 <style type="text/css">
 	ul {background-color: #008bb9;}
@@ -23,19 +72,53 @@
 </header>
 
 <body>
-	<nav class="container navbar navbar-default nabar-static-top">
-	<ul class="nav nav-pills">
-		<li><a class="navbar-brand" href="/verkami/index.php"><img height="32" width="90" src="/verkami/assets/images/logo.png"></a></li>
-		<li><a id="menu-top"> | </a></li>
-		<li><a id="menu-top" href="#"> Proyectos</a></li>
-		<li><a id="menu-top" href="#"> Qué es Verkami</a></li>
-		<li><a id="menu-top" href="#"> FAQ</a></li>
-		<li><a id="menu-top"> | </a></li>
-		<li><a id="menu-top" href="#"> Empieza tu proyecto</a></li>
-		<li><a id="menu-top" href="#"> Mi Perfil</a></li>
-		<li><a id="menu-top" href="#"> Iniciar Sesión/Cerrar</a></li>
-	</ul>
+<nav class="container navbar navbar-default nabar-static-top">
+  <ul class="nav nav-pills">
+    <li><a class="navbar-brand" href="/verkami/index.php"><img height="32" width="90" src="/verkami/assets/images/logo.png"></a></li>
+    <li><a id="menu-top"> | </a></li>
+    <li><a id="menu-top" href="#"> Proyectos </a></li>
+    <li><a id="menu-top" href="#"> Qué es Verkami </a></li>
+    <li><a id="menu-top" href="#"> FAQ </a></li>
+    <li><a id="menu-top"> | </a></li>
+    <li><a id="menu-top" href="#"> Empieza tu proyecto </a></li>
+    <li id="iniciarseseion"><a id="menu-top"  class="btn" data-toggle="modal" data-target="#iniciarSesion"> Iniciar Sesión</a></li>
+    <li id="miperfil" ><a href="/verkami/index.php/perfilPrivado" id="menu-top" class="btn"> Mi Perfil</a></li>
+    <li id="cerrarsesion" ><a id="menu-top" class="btn"> Cerrar Sesión</a></li>
+  </ul>
 </nav>
+
+<div class="modal fade"  id="iniciarSesion" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Cerrar</span></button>
+        <h3 class="modal-title" id="myModalLabel" >Accede a tu cuenta en Verkami</h3>
+      </div>
+      <div id="nuevasesion" class="modal-body">
+            <form role="form" id="formlogin">
+              <div class="form-group">
+                <label for="inciarsesion">E-Mail (*) </label>
+                <input type="text" class="form-control" id="email" placeholder="mail@mail.com" required>
+                <div class="alert alert-danger" hidden="false" name="error" id="erroruser"> Debes rellenar usuario y contraseña.</div>
+
+              </div>
+              <div class="form-group">
+                <label for="password">Contraseña (*) </label>
+                <input type="password" class="form-control" name="password" id="password" placeholder="**********"  required>
+                <div class="alert alert-danger" hidden="false" name="error" id="errorpass"> Debes rellenar usuario y contraseña.</div>
+
+              </div>
+              <div class="form-group" align="right">
+                <a href="/verkami/index.php/registro/"><buton type="button" class="btn btn-primary">Registro</button></a>
+                <input type="button" class="btn btn-success" class="close" id="login" name="login" value="Login" >
+              </div>
+              <div class="alert alert-info" >(*) Campo Obligatorio</div>
+              
+           </form>     
+      </div>
+    </div>
+  </div>
+ </div>
 
 <main class="container">
 	<h1 align="center" >Haciendo Verkami</h1><br>
